c246ba4e51b89db1a0700fcc692c721320fd7215
Pasindu Dilshan <pasindu@thinkcube.com>
2020-04-18 18:01:00 +0530

    Added Lazyloading with React's lazy & suspense

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,4 +1,4 @@
 ## React Material Dashboard RoadMap
 
-- [ ] Adoption of React.lazy & suspense for lazy loading. (Possible in Present)
+- [x] Adoption of React.lazy & suspense for lazy loading. (Possible in Present)
 - [ ] Adoption of Experimental React Concurrent Mode once recommended for production, using suspense for data fetching (Future Adoption)
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,50 @@
-import React, { FC } from "react";
+import React, { FC, Suspense, lazy } from "react";
 import { BrowserRouter as Router, HashRouter, Switch, Redirect } from "react-router-dom";
 import "assets/css/App.css";
 
-import AuthRoute from "components/routes/AuthRoute";
-
-import Landing from "views/Landing";
-
 import { authenticated } from "session/auth";
 
-/* Core Components */
-
-import Login from "views/Auth/SignIn";
+import AuthRoute from "components/routes/AuthRoute";
 import PrivateRoute from "components/routes/PrivateRoute";
 
-import Dashboard from "views/Dashboard";
+import ProgressIndicator from "components/ui/progressIndicator";
+
+const Landing = lazy(() => import("views/Landing"));
+
+/* Core Components */
+const Login = lazy(() => import("views/Auth/SignIn"));
+const Dashboard = lazy(() => import("views/Dashboard"));
 
 const App: FC = () => {
     return (
         <div className="App">
             <Router>
                 <HashRouter>
-                    <Switch>
-                        <AuthRoute
-                            exact
-                            path="/"
-                            component={Landing}
-                            isAuthenticated={authenticated}
-                        />
-                        <AuthRoute
-                            exact
-                            path="/login"
-                            component={Login}
-                            isAuthenticated={authenticated}
-                        />
-                        <PrivateRoute
-                            path="/dashboard"
-                            component={Dashboard}
-                            isAuthenticated={authenticated}
-                        />
-                        {/* Redirect to "URL/#/login" on invalid paths entered as "URL/#/INVALID" */}
-                        <Redirect from="/" to="/login" />
-                        {/* Redirect to "URL/#/login" on invalid paths entered as "URL/#/login/INVALID" */}
-                        <Redirect from="/login" to="/login" />
-                    </Switch>
+                    <Suspense fallback={<ProgressIndicator type="linear" />}>
+                        <Switch>
+                            <AuthRoute
+                                exact
+                                path="/"
+                                component={Landing}
+                                isAuthenticated={authenticated}
+                            />
+                            <AuthRoute
+                                exact
+                                path="/login"
+                                component={Login}
+                                isAuthenticated={authenticated}
+                            />
+                            <PrivateRoute
+                                path="/dashboard"
+                                component={Dashboard}
+                                isAuthenticated={authenticated}
+                            />
+                            {/* Redirect to "URL/#/login" on invalid paths entered as "URL/#/INVALID" */}
+                            <Redirect from="/" to="/login" />
+                            {/* Redirect to "URL/#/login" on invalid paths entered as "URL/#/login/INVALID" */}
+                            <Redirect from="/login" to="/login" />
+                        </Switch>
+                    </Suspense>
                 </HashRouter>
             </Router>
         </div>
