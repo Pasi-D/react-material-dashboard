94623b21b021f46a867ed91f9ec14100f59eb52c
Pasindu Dilshan <pasindu@thinkcube.com>
2020-05-07 15:02:11 +0530

    Copyright section translations

--- src/components/layout/Footer/Copyright.tsx
+++ src/components/layout/Footer/Copyright.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 
 const Copyright: FC = () => {
+    const { t } = useTranslation();
+
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright @"}
-            <Link color="inherit" href={`${process.env.REACT_APP_TRADEMARK_LINK}`}>
-                {`${process.env.REACT_APP_TRADEMARK_NAME} `}
-            </Link>
+            {`${t("copyright")} `}
             {new Date().getFullYear()}
+            <Link color="inherit" href={`${process.env.REACT_APP_TRADEMARK_LINK}`}>
+                {` ${t("organizationName")}`}
+            </Link>
             {"."}
         </Typography>
     );
